7afaae2dc2b82d785f8bbfecc825e70e5fb9342a
luandev79 <learncode74@gmail.com>
2025-05-07 20:11:13 +0700

    first commit

---
+++ bin/index.js
@@ -0,0 +1,41 @@
+#! /usr/bin/env node
+const fs = require('fs');
+const path = require('path');
+const simpleGit = require('simple-git');
+
+const projectName = process.argv[2];
+
+if (!projectName) {
+    console.error('Please provide a project name.');
+    process.exit(1);
+}
+
+const templateRepo = 'https://github.com/luandev79/structure-mern.git';
+
+const projectPath = path.join(process.cwd(), projectName);
+
+if (fs.existsSync(projectPath)) {
+    console.error(`Directory ${projectName} already exists. Please choose a different name.`);
+    process.exit(1);
+}
+
+const cloneRepo = async () => {
+    const git = simpleGit();
+    try {
+        await git.clone(templateRepo, projectPath);
+
+        const gitDir = path.join(projectPath, '.git');
+        if (fs.existsSync(gitDir)) {
+            fs.rmSync(gitDir, { recursive: true, force: true });
+            // recursive: true -> will delete all files and folders inside the .git directory
+            // force: true -> will ignore errors if the directory does not exist, allowing the deletion to proceed without interruption
+        }
+        console.log(`Created project ${projectName} successfully!`);
+        console.log(`Navigate to the project directory: cd ${projectName} \n npm install \n npm run dev`);
+    } catch (error) {
+        console.error('Error cloning repository:', error.message);
+        process.exit(1);
+    }
+};
+
+cloneRepo();
---
+++ readme.md
@@ -0,0 +1,56 @@
+# Fullstack Mern
+
+_A ready-to-use fullstack structure for MERN (MongoDB, Express, React, Node.js) projects._
+
+### 🚀 Features
+
+-   **Backend**: Node.js, Express, MongoDB, Mongoose, JWT authentication, Bcrypt for password hashing, and more.
+-   **Frontend**: React, React Router, Zustand, Axios for API calls, and more.
+
+### 📦 Install
+
+First step is to install the `fullstack-mern` package npm:
+
+```bash
+npx create-fullstack-mern <project-name>
+cd <project-name>
+npm install
+```
+
+**Continue, add file .env with **
+
+1. **Create a `.env` file in the root of your project.**
+2. **Add your environment variables, for example:**
+    ```plaintext
+    MONGODB_URI=<your_mongodb_uri>
+    JWT_SECRET=<your_jwt_secret>
+    PORT=5000
+    ```
+
+### 📁 Project Structure
+
+```bash
+<project-name>/
+    ├── backend/
+    │ ├── config/
+    │ ├── controllers/
+    │ ├── middleware/
+    │ ├── models/
+    │ ├── routes/
+    │ ├── utils/
+    │ └── server.js
+    ├── frontend/
+    │ ├── public/
+    │ ├── src/
+    │ │ ├── components/
+    │ │ ├── helpers/
+    │ │ ├── hooks/
+    │ │ ├── layouts/
+    │ │ ├── stores/
+    │ │ ├── pages/
+    │ │ ├── App.jsx
+    │ │ ├── index.jsx
+    │ │ └── index.css
+    │ └── index.html
+    └── README.md
+```
